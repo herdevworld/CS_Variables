9edc8ecb6b7def16c259c0ff898d570d2d1bfddc
desiree <developwithdesiree@gmail.com>
2025-04-26 22:27:26 -0500

    Add project files.

---
+++ CS_Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿// Lesson: Creating Variables in C# (.NET 8 Style)
+
+// TODO 1: Create an integer variable called 'age' and set it to 25.
+int age = 25;
+
+// TODO 2: Create a string variable called 'firstName' and set it to your name.
+string firstName = "YourName";
+
+// TODO 3: Create a bool variable called 'isStudent' and set it to true or false.
+bool isStudent = true;
+
+// TODO 4: Create a double variable called 'gpa' and set it to a decimal value like 3.5.
+double gpa = 3.5;
+
+// TODO 5: Print all the variables to the Console.
+Console.WriteLine("Age: " + age);
+Console.WriteLine("First Name: " + firstName);
+Console.WriteLine("Is Student: " + isStudent);
+Console.WriteLine("GPA: " + gpa);
+
+// ✅ Bonus Challenge:
+// Combine the first name and age into a sentence and print it!
+Console.WriteLine($"Hello, my name is {firstName} and I am {age} years old.");
